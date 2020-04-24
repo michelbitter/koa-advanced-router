82a8dfc7b93d5d4f7476e175227d0d8b2e0f00d3
michelbitter <michel@michelbitter.nl>
2020-04-24 15:25:31 +0200

    clean the way how we export tyings

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import {RouterOptions as ROptions} from './router/router.interfaces'
 import RouterClass from './router/router'
-import {Params as IParams, Methods as IMethods, RouteObj, Param as IParam} from './layer/layer.interfaces'
-import {DefaultState, DefaultContext} from 'koa'
+import {RouterOptions} from './router/router.interfaces'
 
-export type RouterOptions = ROptions
-export type Param<StateT = DefaultState, CustomT = DefaultContext, TParam = unknown> = IParam<StateT, CustomT, TParam>
-export type Params = IParams
-export type Methods = IMethods
-export type Route = RouteObj
+export {RouterOptions} from './router/router.interfaces'
+export {Params, Methods, RouteObj, Param} from './layer/layer.interfaces'
 
 export function Router(opts?: RouterOptions) {
   return RouterClass.factory(opts)
