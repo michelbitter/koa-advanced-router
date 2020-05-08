c59f5fb55e316affd750ac8434ceda2136952a13
michelbitter <michel@michelbitter.nl>
2020-05-08 11:38:52 +0200

    Make RouterClass publicly available

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import RouterClass from './router/router'
 import {RouterOptions} from './router/router.interfaces'
+import RouterClass from './router/router'
 
 export {RouterOptions, VersionOptions, VersionMatchingFunction} from './router/router.interfaces'
 export {Params, Methods, RouteObj, Param} from './layer/layer.interfaces'
 export {Options as RouteOptions} from './interfaces'
 
-export function Router(opts?: RouterOptions) {
+export const Router = RouterClass
+export default function (opts?: RouterOptions) {
   return RouterClass.factory(opts)
 }
-
-export default Router
