cc8b3bb45dbaf87a35ff3ae8f2e72ea94e99842c
michelbitter <michel@michelbitter.nl>
2020-05-08 08:34:05 +0200

    export extra Typescript Typings
    Signed-off-by: michelbitter <michel@michelbitter.nl>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import RouterClass from './router/router'
 import {RouterOptions} from './router/router.interfaces'
 
-export {RouterOptions} from './router/router.interfaces'
+export {RouterOptions, VersionOptions, VersionMatchingFunction} from './router/router.interfaces'
 export {Params, Methods, RouteObj, Param} from './layer/layer.interfaces'
+export {Options as RouteOptions} from './interfaces'
 
 export function Router(opts?: RouterOptions) {
   return RouterClass.factory(opts)
